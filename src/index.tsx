d09552dd7ec1fba45c527367a03a3f3b7d050a14
sam <syed.simanta06@gmail.com>
2020-09-13 17:16:17 +0600

    added

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom'
 import App from './components/App/App'
 import './index.css'
 
-
-
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
-
